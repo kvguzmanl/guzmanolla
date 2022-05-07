2f9faa2cbcdb78d2cb44e209398307af666a43bc
Edgar Alonso Olla <alonsoolla@hotmail.com>
2022-05-07 14:27:36 -0600

    Migración para tablas Sellers y Products

--- database/migrations/2022_05_07_194644_create_sellers_table.php
+++ database/migrations/2022_05_07_194644_create_sellers_table.php
@@ -15,7 +15,10 @@ class CreateSellersTable extends Migration
     {
         Schema::create('sellers', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('dui', 10);
+            $table->string('direccion', 300);
+            $table->string('nit', 19);
+            $table->foreignId('id_usuario')->references('id')->on('users');
         });
     }
 
